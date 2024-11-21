eb789d330b6fefb943d9bf6ac48dc382cbba4607
k0miker <165369924+k0miker@users.noreply.github.com>
2024-11-21 15:50:54 +0100

    update

--- src/components/globe.jsx
+++ src/components/globe.jsx
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import Globe from "react-globe.gl";
+import earthMarble from "../assets/images/earth-blue-marble.jpg";
+
 
 const GlobeComponent = () => {
   const globeEl = useRef();
@@ -7,7 +9,7 @@ const GlobeComponent = () => {
   useEffect(() => {
     if (globeEl.current) {
       globeEl.current.controls().autoRotate = true;
-      globeEl.current.controls().autoRotateSpeed = 0.2;
+      globeEl.current.controls().autoRotateSpeed = -0.5;
     }
   }, []);
 
@@ -15,7 +17,7 @@ const GlobeComponent = () => {
     <div className="w-full h-full absolute ">
       <Globe
         ref={globeEl}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        globeImageUrl={earthMarble}
         backgroundColor="rgba(0,0,0,0)"
       />
     </div>
--- src/components/output.jsx
+++ src/components/output.jsx
@@ -1,41 +1,44 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function output() {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get("https://ghoapi.azureedge.net/api/GHO");
-          setData(response.data.value); // Die Daten befinden sich im `value`-Array
-          setLoading(false);
-        } catch (error) {
-          console.error("Fehler beim Abrufen der Daten:", error);
-          setLoading(false);
-        }
-      };
-      fetchData();
-    }, []);
-  
-    if (loading) {
-      return <p>Lädt...</p>;
-    }
-  
-    return (
-      <div>
-        <h1>Todesdaten Dashboard</h1>
-        <ul>
-          {data.map((item) => (
-            <li key={item.IndicatorCode}>
-              {item.IndicatorName} - {item.Country}: {item.Value}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
+//   const [data, setData] = useState([]);
+//   const [loading, setLoading] = useState(true);
 
-export default output
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get(
+//           "https://cors-anywhere.herokuapp.com/https://ghoapi.azureedge.net/api/GHO"
+//         );
+
+//         setData(response.data.value); // Die Daten befinden sich im `value`-Array
+//         setLoading(false);
+//       } catch (error) {
+//         console.error("Fehler beim Abrufen der Daten:", error);
+//         setLoading(false);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   if (loading) {
+//     return <p>Lädt...</p>;
+//   }
+
+  return (
+    <div>
+      {/* <h1>Todesdaten Dashboard</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.IndicatorCode}>
+            {item.IndicatorName} - {item.Country}: {item.Value}
+          </li>
+        ))}
+      </ul> */}
+    </div>
+  );
+}
+
+export default output;
